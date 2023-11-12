588646ca17a3a5ec2d85da0dfaf9ed293896c5ab
ata_turkoglu <ata.turkoglu@reengen.com>
2023-11-12 12:43:55 +0300

    add app.tsx file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
 
-export class App extends React.Component {
+interface AppProps {
+    todos: Todo[];
+    fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+    onBtnClick = (): void => {
+        this.props.fetchTodos();
+    };
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title}</div>;
+        });
+    }
+
     render() {
-        return <div>TEST</div>;
+        return (
+            <div>
+                <button onClick={this.onBtnClick}>GET TODO LIST</button>
+                {this.renderList()}
+            </div>
+        );
     }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+    return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
